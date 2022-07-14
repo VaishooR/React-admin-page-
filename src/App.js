bc9af0503c3484c059dd96a210a1c09a1590fc4b
VaishooR <vaishooramkrish@gmail.com>
2022-07-14 17:08:38 +0530

    practice React admin page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import './assets/sb-admin-2.min.css';
+import Sidebar from './components/1.Sidebar';
+import Topbar from './components/2.Topbar';
+import Dashboard from './components/3.Dashboard';
+import Footer from './components/4.Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div id="wrapper">
+      <Sidebar/>
+          <div id="content-wrapper" class="d-flex flex-column">
+              <div id="content">
+              <Topbar/>
+                    <div class="container-fluid">
+                    <Dashboard/>
+                    </div>
+              </div> 
+          <Footer/>   
+          </div>     
+      </div>
+    </>
   );
 }
 
